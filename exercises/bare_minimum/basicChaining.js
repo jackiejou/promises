0366c8d5ba4fbc91da5729021fcfeaa4dbb610f8
jackiejou <joujackie@gmail.com>
2017-12-21 11:29:09 -0800

    Complete basic reqs for promises

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,11 +10,37 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
+var getGithubProfile = require('./promisification').getGitHubProfileAsync;
+var pluckFirstLine = require('./promiseConstructor').pluckFirstLineFromFileAsync;
 
-
+var writeToFile = function(path, data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(path, JSON.stringify(data), (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(console.log('File written'));
+      }
+    });
+  });
+};
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
   // TODO
+
+  // read from readFile
+  return pluckFirstLine(readFilePath)
+    .then(function(user) {
+      return getGithubProfile(user);
+    })
+    .then(function(html) {
+      return writeToFile(writeFilePath, html);
+    })
+    .catch(function(err) {
+      console.log('Error in promise chain');
+    });
+  // fetch user page
+  // write body of response to writeFile
 };
 
 // Export these functions so we can test them
--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,12 +6,28 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
+  fs.readFile(filePath, function(err, data) {
+    if (err) {
+      callback(err, null);
+    } else {
+      data += '';
+      data = data.split('\n')[0];
+      callback(err, data);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function (url, callback) {
+  request(url, (err, response, body) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(err, response.statusCode);
+    }
+  });
   // TODO
 };
 
